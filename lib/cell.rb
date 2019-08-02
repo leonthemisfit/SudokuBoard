0ee06ef7b4bfed4d922092a3a8b4fbf1bd539e9b
leonthemisfit <leonthemisfit@gmail.com>
2019-08-02 09:29:05 -0400

    Create Cell class for individual cells
    The cell class created here is the building block of the board system,
    designed to represent a cell on the board and it's information including
    value and pencil marks.
    
    On branch master
    Changes to be committed:
    	new file:   lib/cell.rb

---
+++ lib/cell.rb
@@ -0,0 +1,60 @@
+# frozen_string_literal: true
+
+require_relative 'errors'
+
+module Sudoku
+  class Cell
+    attr_reader :value
+
+    def initialize(value = nil, constant = false)
+      @value = value
+      @constant = constant
+      @marks = []
+    end
+
+    def to_s
+      if value.nil?
+        '*'
+      else
+        value.to_s
+      end
+    end
+
+    def value=(val)
+      raise Errors::Cell::ConstantError if constant?
+
+      if val == '*'
+        clear
+      else
+        raise Errors::Cell::ValueTypeError unless val.is_a? Integer
+        raise Errors::Cell::ValueError unless (1..9).include?(val)
+
+        @value = val
+        @marks = []
+        @constant = true
+      end
+    end
+
+    def constant?
+      @constant
+    end
+
+    def clear(rm_constant = true)
+      @value = nil
+      @marks = []
+      @constant = false if rm_constant
+    end
+
+    def mark(val)
+      raise Errors::Cell::ValueError unless (1..9).include?(val)
+
+      @marks << val unless @marks.include?(val)
+    end
+
+    def erase(val)
+      raise Errors::Cell::ValueError unless (1..9).include?(val)
+
+      @marks.delete(val)
+    end
+  end
+end
